6b86724b030b6038554d84fbd2522aa5749eaf47
Robert <robert@locastic.com>
2024-08-05 15:23:59 +0200

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import * as TaskManager from 'expo-task-manager';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+export const LOCATION_TASK_NAME = 'background-location-task';
+
+TaskManager.defineTask(LOCATION_TASK_NAME, ({ data, error }) => {
+  if (error) {
+    return;
+  }
+  if (data) {
+    console.log('NAVIGATION data:', data);
+  }
+});
+
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  async function requestPermissions() {
+    const requestForeground = Location.requestForegroundPermissionsAsync;
+    const requestBackground = Location.requestBackgroundPermissionsAsync;
+
+    const foregroundRequest = await requestForeground();
+    if (foregroundRequest.granted) {
+      const backgroundRequest = await requestBackground();
+      if (backgroundRequest.granted) {
+        await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+          accuracy: Location.Accuracy.Highest,
+          activityType: Location.LocationActivityType.Fitness,
+          foregroundService: {
+            notificationTitle: 'Location',
+            notificationBody: 'Tracking your location',
+          },
+        });
+      }
+    }
+    return false;
+  }
+
+  React.useEffect(() => {
+    requestPermissions();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
